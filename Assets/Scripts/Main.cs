45e68122c24143722839f9431d3f6d7aad1107f8
mircheus <mircheuss@yandex.ru>
2023-08-28 17:46:22 +0200

    init

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] private GameObject gameObject;
+    [SerializeField] private GameObject gameObject2;
+
+    private Animator _animator;
+}
